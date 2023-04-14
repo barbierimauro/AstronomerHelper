98eeead120742377db8fb7c649bb3a248c59a3e0
Mauro Barbieri <87614716+barbierimauro@users.noreply.github.com>
2023-04-14 09:29:44 +0200

    Update get_dpid.sh

--- ep3fsck/get_dpid.sh
+++ ep3fsck/get_dpid.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-url="https://dataportal.eso.org/dataportal_new/file/"
-wdir=$PWD
+#
+# Script for downloading multiple files from the ESO archive
+# It is necessary to known the name of the files and have stored them
+# in a list one per line
+
+
+#BEGIN PERSONAL SETUP
+
+# modify this variable with your file name
 mylist="path_and_filename_of_DPID_to_retrieve"  
 # This file need to contain only 1 column
 # the content of the column need to be a valid DPID of a ESO archive file
+
+# adjust the path
+outdir="telescope/instrument/data_type"  
+
+# you can modify this number in integers from 1 to 99
+# after downloading this precentage of files over the total number 
+# it will be displayed a notification on the screen
+# no need to modify in general case, but useful when downloading thousands of files
+percento=10
+
+if ! [[ $percento =~ ^[0-9]+$ ]]; then
+    echo "Error: percentual is not numeric"
+elif (( percento < 1 || percento > 99 )); then
+    echo "Error: percentual is not between 1 and 99"
+fi
+
+
+#END PERSONAL SETUP
+
+url="https://dataportal.eso.org/dataportal_new/file/"
+wdir=$PWD
 
 function getfits() {
 # Count the total number of lines in the mylist file
@@ -11,7 +39,7 @@ N=$(wc -l < "$mylist")
 
 # Initialize the counter and the last progress variable
 i=0
-last_progress=-10
+last_progress=-$percento
 
 # Read the mylist file line by line
 while read -r dp_id; do
@@ -21,14 +49,13 @@ while read -r dp_id; do
 # Calculate the percentage of progress
 percentage=$((i * 100 / N))
 
-# Check if the progress is a multiple of 10%
-if ((percentage / 10 > last_progress / 10)); then
+# Check if the progress is a multiple of $percento 
+if ((percentage / $percento > last_progress / $percento)); then
 echo "Progress: $percentage%"
 last_progress=$percentage
 fi
 
 # Create the directory if it doesn't exist
-outdir="telescope/instrument/data_type"  # adjust the path
 mkdir -p "$outdir"
 
 # Download the file
@@ -47,7 +74,8 @@ cd "$wdir"
 fi
 
 # Create a header file for the FITS file
-dfits -x 0 "$outdir/$dp_id.fits" > "$outdir/$dp_id.hdr"
+# not needed in normal operations, but can be activated uncommenting the following line
+# dfits -x 0 "$outdir/$dp_id.fits" > "$outdir/$dp_id.hdr"
 
 done < "mylist"
 
